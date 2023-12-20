148058ac05bde0046fe9058a7b9c62b291a43b4c
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2023-12-20 14:05:05 +0300

    new

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,44 +1,56 @@
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
-import {useForm} from 'react-hook-form'
-import { addDoc,collection } from 'firebase/firestore'
-import {auth, db} from '../../config/firebase'
-import { useAuthState } from 'react-firebase-hooks/auth'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth, db } from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+
 interface CreateFormData {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
-export const CreateForm = () =>{
+export const CreateForm = () => {
+  const navigate = useNavigate(); // Call the hook function
+  const [user] = useAuthState(auth);
 
-const [user] = useAuthState(auth)
+  const schema = yup.object().shape({
+    title: yup.string().required('You Must add a title.'),
+    description: yup.string().required('You Must add a Description to your post.'),
+  });
 
-const schema = yup.object().shape({
-    title: yup.string().required("You Must add a title.  "),
-    description: yup.string().required("You Must add a Description to your post."),
-});   
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateFormData>({
+    resolver: yupResolver(schema),
+  });
 
-const  {register,
-     handleSubmit,
-      formState: {errors},} = useForm<CreateFormData>({
-resolver: yupResolver(schema),
-});
+  const postsRef = collection(db, 'posts');
 
-const postsRef = collection (db, "posts")
+  const onCreatePost = async (data: CreateFormData) => {
+    try {
+      await addDoc(postsRef, {
+        ...data,
+        username: user?.displayName,
+        userId: user?.uid,
+      });
+      navigate('./'); // Redirect after successful submission
+    } catch (error) {
+      // Handle errors appropriately, e.g., display an error message
+      console.error(error);
+    }
+  };
 
-const onCreatePost = async (data:CreateFormData) => {
-await addDoc(postsRef, {
-...data,//spread operator
-    username:user?.displayName,
-    userId: user?.uid,
-})
-}
-
-return <form onSubmit={handleSubmit(onCreatePost)}>
-    <input  placeholder="Title.." {...register("title")}/>
-    <p style={{color:"red"}}>{errors.title?.message}</p>
-    <textarea placeholder="Description.." {...register("description")} />
-    <p style={{color:"red"}}>{errors.description?.message}</p>
-    <input type='submit'/>
-</form>
-}
+  return (
+    <form onSubmit={handleSubmit(onCreatePost)}>
+      <input placeholder="Title.." {...register('title')} />
+      <p style={{ color: 'red' }}>{errors.title?.message}</p>
+      <textarea placeholder="Description.." {...register('description')} />
+      <p style={{ color: 'red' }}>{errors.description?.message}</p>
+      <input type="submit" className="submitForm" />
+    </form>
+  );
+};
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import {db} from "../config/firebase"
+import { useState } from "react";
+import {collection, getDocs} from 'firebase/firestore'
 export const Main =()=>{
-    return(
-        <div>Home page</div>
+   const [postsList, setPostLists] = useState(null)
+        const postRef = collection(db, "posts");
+        return(<div>Home page</div>
     )
 }
