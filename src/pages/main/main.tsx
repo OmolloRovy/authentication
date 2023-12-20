0c917521ff576c512987c5d00099794a4675f276
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2023-12-20 14:05:45 +0300

    new

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { db } from "../../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { Post } from "./post";
+
+export interface Post {
+  id: string;
+  userId: string;
+  title: string; // Corrected typo: "tittle" -> "title"
+  username: string;
+  description: string;
+}
+
+export const Main = () => {
+  const [postsList, setPostsList] = useState<Post[] | null>(null); // Corrected typo: "PostLists" -> "postsList"
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const postsRef = collection(db, "posts");
+      const data = await getDocs(postsRef);
+      setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
+    };
+
+    getPosts();
+  }, []); // Empty dependency array to fetch posts only once
+
+  return (
+    <div>
+      {postsList?.map((post) => (
+        <Post key={post.id} post={post} /> // Added key for React efficiency
+      ))}
+    </div>
+  );
+};
