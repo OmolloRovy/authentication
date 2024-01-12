964eca7a655f27cfe77e38222bc8c33f9f19d847
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2024-01-12 11:42:44 +0300

    new

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, query, where,getDocs } from "firebase/firestore";
+import { addDoc, collection, query, where,getDocs,deleteDoc, doc } from "firebase/firestore";
 import {Post as IPost} from "./main"
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -23,11 +23,34 @@ export const Post = (props: Props) =>{
         setLikes(data.docs.map((doc) => ({userId: doc.data().userId })))
     };
     const addLike = async () => {
+        try {
         await addDoc(likesRef, { userId:user?.uid , postId: post.id} );
         if (user){
       setLikes( (prev)=>prev ? [...prev, {userId: user.uid}] : [{userId: user.uid}]
       );
     }
+} catch (err){
+    console.log(err);
+}
+    };
+
+    const  removeLike = async () => {
+        try {
+
+            const likeToDeleteQuery = query(likesRef, 
+                where("postId", "==", post.id),
+                 where("userId", "==", user?.uid)
+                 );
+                 const likeToDeleteData = await getDocs(likeToDeleteQuery)
+            const likeToDelete = doc(db, "likes", likeToDeleteData.docs[0].id);
+        await deleteDoc(likeToDelete);
+    //     if (user){
+    //   setLikes( (prev)=>prev ? [...prev, {userId: user.uid}] : [{userId: user.uid}]
+    //   );
+    // }
+} catch (err){
+    console.log(err);
+}
     };
 
      const hasUserLiked = likes?.find((like)=>like.userId=== user?.uid)
@@ -49,7 +72,8 @@ export const Post = (props: Props) =>{
 
     <div className="footer">
         <p> @{post.username}</p>
-        <button onClick={addLike}> { hasUserLiked ? <>&#128078;</> :  <>&#128077;</> } </button>
+        <button onClick={ hasUserLiked ? removeLike :  addLike}>
+             { hasUserLiked ? <>&#128078;</> :  <>&#128077;</> }{""} </button>
 
    {likes && <p> Likes:  {likes?.length}</p>}
     </div>
